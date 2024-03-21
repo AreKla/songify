37364ce4df5848e75fda60bdb5ba00961c1292c8
klakd <arkadiusz.klak@gmail.com>
2024-03-21 18:08:58 +0100

    Refactored

---
+++ src/main/java/com/songify/SongViewController.java
@@ -0,0 +1,30 @@
+package com.songify;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Controller
+public class SongViewController {
+
+    private Map<Integer, String> database = new HashMap<>();
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/view/songs")
+    public String songs(Model model) {
+        database.put(1, "Podsiadło song1");
+        database.put(2, "Pezet song2");
+        database.put(3, "O.S.T.R. song3");
+        database.put(4, "Magik song4");
+        model.addAttribute("songMap", database);
+        return "songs";
+    }
+}
--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -13,32 +13,10 @@ import java.util.stream.Collectors;
 @Log4j2
 public class SongsController {
 
-    Map<Integer, String> database = new HashMap<>();
+    Map<Integer, String> database = new HashMap<>(Map.of(1, "Podsiadło song1", 2, "Pezet song2", 3, "O.S.T.R. song3", 4, "Magik song4"));
 
     @GetMapping("/songs")
-    public ResponseEntity<SongResponseDto> getAllSongs() {
-        database.put(1, "Podsiadło song1");
-        database.put(2, "Pezet song2");
-        SongResponseDto response = new SongResponseDto(database);
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/songs/{id}")
-    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer id) {
-        String song = database.get(id);
-        if (song == null) {
-            return ResponseEntity.notFound().build();
-        }
-        SingleSongResponseDto response = new SingleSongResponseDto(song);
-        return ResponseEntity.ok(response);
-    }
-
-    @GetMapping("/songs?id=")
-    public ResponseEntity<SongResponseDto> getSongByParam(@RequestParam Integer limit) {
-        database.put(1, "Podsiadło song1");
-        database.put(2, "Pezet song2");
-        database.put(3, "O.S.T.R. song3");
-        database.put(4, "Magik song4");
+    public ResponseEntity<SongResponseDto> getAllSongs(@RequestParam(required = false) Integer limit) {
         if (limit != null) {
             Map<Integer, String> limitedMap = database.entrySet().stream().limit(limit).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
             SongResponseDto response = new SongResponseDto(limitedMap);
@@ -48,12 +26,10 @@ public class SongsController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/songsHeaders/{headerId}")
-    public ResponseEntity<SingleSongResponseDto> getByHeader(@PathVariable Integer headerId, @RequestHeader(
-            name = "Request-Id",
-            required = false) String requestId) {
+    @GetMapping("/songs/{id}")
+    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer id, @RequestHeader(required = false) String requestId) {
         log.info(requestId);
-        String song = database.get(headerId);
+        String song = database.get(id);
         if (song == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
